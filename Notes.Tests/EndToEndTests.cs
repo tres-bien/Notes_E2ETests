dbf4f65a94757f5748673a333191cdb5939a366c
tres-bien <denischudinov1212@gmail.com>
2023-10-07 18:44:01 +0300

    Add project files.

---
+++ Notes.Tests/EndToEndTests.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.NetworkInformation;
+
+namespace Notes.Tests
+{
+    public class EndToEndTests
+    {
+        [Test]
+        public async Task TestCreateAndUpdateNote()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            var chrome = playwright.Chromium;
+            var browser = await chrome.LaunchAsync(new() { Headless = false });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://localhost:7209/");
+
+            // Create a new note
+            await page.FillAsync("#title", "Test Title");
+            await page.FillAsync("#content", "Test Content");
+            await page.ClickAsync(".btn-success");  // Click on the "Create" button
+
+            // Wait for the note to appear in the table
+            await page.WaitForSelectorAsync("td:has-text('Test Title')");
+
+            // Search for the note by its title
+            await page.FillAsync(".form-control[placeholder='Search by Title or Content']", "Test Title");
+
+            // Wait for the dropdown-content to display the note
+            await page.WaitForSelectorAsync("ul.dropdown-content li:has-text('Test Title')");
+
+            // Click on the searched note from the dropdown
+            await page.ClickAsync("ul.dropdown-content li:has-text('Test Title')");
+
+            // Click on the last "Delete Note" button on the page
+            await page.ClickAsync("button:has-text('Delete Note')");
+            //await page.ClickAsync("td:has-text('Test Title') + button:has-text('Delete Note')");
+
+            // Verify the note is deleted (waiting for it to disappear from the table)
+            await page.WaitForSelectorAsync("td:has-text('Test Title')", new() { State = WaitForSelectorState.Detached });
+
+            await browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task TestEditNote()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            var chrome = playwright.Chromium;
+            var browser = await chrome.LaunchAsync(new() { Headless = false });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://localhost:7209/");
+
+            // Create a new note for the purpose of this test
+            await page.FillAsync("#title", "Edit Test Title");
+            await page.FillAsync("#content", "Edit Test Content");
+            await page.ClickAsync(".btn-success");  // Click on the "Create" button
+
+            // Wait for the note to appear in the table
+            await page.WaitForSelectorAsync("td:has-text('Edit Test Title')");
+
+            // Click on the "Edit Note" button
+            await page.ClickAsync("td:has-text('Edit Test Title') + td + td button:has-text('Edit Note')");
+
+            // Fill in the updated details
+            await page.FillAsync("td input[id='editTitle']", "Updated Test Title");
+            await page.FillAsync("td textarea[id='editContent']", "Updated Test Content");
+
+            // Click on "Apply Changes" button
+            await page.ClickAsync("td:has-text('Updated Test Title') + td + td button:has-text('Apply Changes')");
+
+            // Validate that the changes have been applied
+            await page.WaitForSelectorAsync("td:has-text('Updated Test Title')");
+
+            // Clean up by deleting the note we just edited
+            await page.ClickAsync("td:has-text('Updated Test Title') + td + td button:has-text('Delete Note')");
+
+            // Verify the note is deleted
+            await page.WaitForSelectorAsync("td:has-text('Updated Test Title')", new() { State = WaitForSelectorState.Detached });
+
+            await browser.CloseAsync();
+        }
+    }
+}
---
+++ Notes.Tests/PlaywrightApiTests.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes_WebApp.Shared;
+using System.Text.Json;
+
+namespace Notes.Tests
+{
+    internal class PlaywrightApiTests
+    {
+        private IPlaywright _playwright;
+        private IAPIRequestContext _requestContext;
+
+        [SetUp]
+        public async Task InitializeTests()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _requestContext = await _playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://localhost:7209/",
+                IgnoreHTTPSErrors = true,
+            });
+        }
+
+        [Test]
+        [Order(1)]
+        public async Task GetAllNotesReturnsData()
+        {
+            var responce = await _requestContext.GetAsync("api/Notes");
+            var data = await responce.JsonAsync();
+
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        [Order(2)]
+        public async Task GetSingleNoteReturnsData()
+        {
+            var responce = await _requestContext.GetAsync("api/Notes/1");
+
+            var data = await responce.JsonAsync();
+
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        [Order(3)]
+        public async Task CanCreateNewNote()
+        {
+            var newNoteContent = new Note
+            {
+                Title = "E2E Test",
+                Content = "This is an E2E test content",
+                Date = DateTime.Now,
+                IsExpanded = false,
+                IsEditing = false
+            };
+
+            var responce = await _requestContext.PostAsync("api/Notes", new APIRequestContextOptions()
+            {
+                DataObject = newNoteContent
+            });
+
+            Assert.That(responce.Status, Is.EqualTo(200));
+        }
+
+        [Test]
+        [Order(4)]
+        public async Task CanUpdateNoteTitle()
+        {
+            var get = await _requestContext.GetAsync("api/Notes");
+            var jsonString = await get.TextAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Note> notes = JsonSerializer.Deserialize<List<Note>>(jsonString, options)!;
+            string targetTitle = "E2E Test";
+            Note targetNote = notes.FirstOrDefault(n => n.Title == targetTitle)!;
+
+            if (targetNote!.Title != targetTitle)
+            {
+                Assert.Fail("Note not found");
+            }
+
+            var updatedNote = new Note
+            {
+                Title = "Updated Title",
+                Content = "Updated Content",
+                Date = DateTime.UtcNow,
+                IsExpanded = true,
+                IsEditing = false
+            };
+
+            var responce = await _requestContext.PutAsync($"api/Notes/{targetNote.Id}", new APIRequestContextOptions()
+            {
+                DataObject = updatedNote
+            });
+
+            Assert.Positive(responce.Status);
+        }
+
+
+        [Test]
+        [Order(5)]
+        public async Task CanDeleteUpdatedNote()
+        {
+            var get = await _requestContext.GetAsync("api/Notes");
+            var jsonString = await get.TextAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Note> notes = JsonSerializer.Deserialize<List<Note>>(jsonString, options)!;
+            string targetTitle = "Updated Title";
+            Note targetNote = notes.FirstOrDefault(n => n.Title == targetTitle)!;
+
+            if (targetNote!.Title != targetTitle)
+            {
+                Assert.Fail("Note not found");
+            }
+            Assert.That(targetNote.Title, Is.EqualTo(targetTitle));
+            var responce = await _requestContext.DeleteAsync($"api/Notes/{targetNote.Id}");
+            Assert.Positive(responce.Status);
+        }
+    }
+}
